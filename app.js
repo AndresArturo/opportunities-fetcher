af568a724a7e5e6f13708ed60e6490e6c09f9841
Andrés Arturo <griyftan@gmail.com>
2016-11-15 20:54:11 +0100

    Tested app

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
 var db = require("./DB");
+var Fetcher = require("./OppsFetcher");
+var emailer = require("./Emailer");
 
 
-db.saveOpps([{ a: 1, _id: 123 }, { a: 2, _id: 321 }], (err) => {
-    if (err) {
-        console.error(err);
-        db.close();
-    } else
-        db.getOpps((err, opps) => {
-            if (err)
-                console.error(err);
-            else
-                opps.forEach((opp) => console.log(opp));
+
+function OpportunitiesChecker(knownOpps) {
+    this.newOpps = [];
+    this.knownOpps = knownOpps || [];
+}
+
+OpportunitiesChecker.prototype.process = function(err, page) {
+    var pageNewOpps;
+
+    if (this.handleError(err)) {
+        pageNewOpps = page.data.filter((opp) => this.knownOpps.filter((known) => known.id == opp.id).length == 0);
+        Array.prototype.push.apply(this.newOpps, pageNewOpps);
+    }
+}
+
+OpportunitiesChecker.prototype.updateOpps = function() {
+    emailer.sendNewOpportunities(this.newOpps);
+
+    if (this.newOpps.length > 0)
+        db.saveOpps(this.newOpps, (err) => {
+            this.handleError(err);
             db.close();
         });
+    else
+        db.close();
+}
+
+OpportunitiesChecker.prototype.handleError = function(err) {
+    if (err)
+        emailer.sendError(err);
+    return !err;
+}
+
+
+db.getOpps((err, opps) => {
+    var fetcher;
+    var oppsChecker;
+
+    if (err) {
+        emailer.sendError(err);
+        db.close();
+    } else {
+        oppsChecker = new OpportunitiesChecker(opps);
+        fetcher = new Fetcher((err, page) => oppsChecker.process(err, page));
+        fetcher.on("end", () => oppsChecker.updateOpps());
+        fetcher.getPages();
+    }
 });
--- oppsFetcher.js
+++ oppsFetcher.js
@@ -7,9 +7,9 @@ function getPage(page, callback) {
     const options = {
         protocol: "https:",
         hostname: "gis-api.aiesec.org",
-        path: `/v2/opportunities?access_token=e316ebe109dd84ed16734e5161a2d236d0a7e6daf499941f7c110078e3c75493&per_page=12&page=${page}&filters[earliest_start_date]="2016-10-10"&filters[home_mcs][]=1596&filters[programmes][]=2`,
+        path: `/v2/opportunities?access_token=e316ebe109dd84ed16734e5161a2d236d0a7e6daf499941f7c110078e3c75493&per_page=12&page=${page}&filters[earliest_start_date]="2016-10-10"&filters[programmes][]=2&filters[home_mcs][]=1596&filters[home_mcs][]=1594&filters[home_mcs][]=1613`,
         headers: {
-            "Referer": "https://opportunities.aiesec.org/programmes/GT?earliest_start_date=%222016-10-10%22&home_mcs%5B%5D=1596",
+            "Referer": "https://opportunities.aiesec.org/programmes/GT?earliest_start_date=%222016-10-10%22&home_mcs%5B%5D=1596,1594,1613",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:49.0) Gecko/20100101 Firefox/49.0"
         }
     };
@@ -35,7 +35,7 @@ function getPage(page, callback) {
                 callback(error, jsonData);
             });
         }
-    }).on("error", (e) => {
+    }).on("error", () => {
         callback(new Error(`Page ${page}: Can't reach AIESEC's server`));
     });
 }
@@ -43,30 +43,32 @@ function getPage(page, callback) {
 
 function OppsFetcher(callback) {
     this.callback = callback;
-    this.pageCount = 1;
+    this.totalPages = 1;
+    this.pagesCount = 0;
 }
 
 util.inherits(OppsFetcher, EventEmitter);
 
+OppsFetcher.prototype.processPage = function(err, page) {
+    this.callback(err, page);
+
+    if ((++this.pagesCount) == this.totalPages)
+        this.emit("end");
+}
+
 OppsFetcher.prototype.getPages = function() {
     getPage(1, (err, page) => {
         if (err)
-            this.callback(err);
+            this.processPage(err);
         else if (page.paging.total_items == 1 || page.paging.total_items != page.paging.total_pages) {
-            this.callback(null, page);
-            this.pageCount = page.paging.total_pages;
+            this.totalPages = page.paging.total_pages;
+            this.processPage(null, page);
 
-            for (var pageI = 2; pageI <= this.pageCount; pageI++) {
-                if (pageI == this.pageCount)
-                    getPage(pageI, (err, page) => {
-                        this.callback(err, page);
-                        this.emit("end");
-                    });
-                else
-                    getPage(pageI, this.callback);
-            }
+            for (var pageI = 2; pageI <= this.totalPages; pageI++)
+                getPage(pageI, (err, page) => this.processPage(err, page));
+
         } else
-            this.callback(new Error("Page 1: JSON illogical answer (total_items = total_pages)"));
+            this.processPage(new Error("Page 1: JSON illogical answer (total_items = total_pages)"));
     });
 }
 
