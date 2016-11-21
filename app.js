6503b45caecb59570e396a3c406b41e408783f22
Andrés Arturo <griyftan@gmail.com>
2016-11-21 18:47:42 +0100

    Filter HTTP requests. Filter the requests to only respond to the AWS SQS events.

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const Processor = require("./Processor");
 const http = require("http");
 
 
-http.createServer((req, res) =>
-    Processor((success) => {
-        console.log(`${new Date()}  ${req.method} --- ${success}  ****************************************************`);
-        res.writeHead(success ? 200 : 500);
+http.createServer((req, res) => {
+    if (req.method == "POST" && req.url == "/trigger")
+        Processor((success) => {
+            console.log(`${new Date()}  ${req.method} --- ${success}  ****************************************************`);
+            res.writeHead(success ? 200 : 500);
+            res.end();
+        });
+    else {
+        res.writeHead(400);
         res.end();
-    })
-).listen(process.env.PORT || 3000);
+    }
+}).listen(process.env.PORT || 3000);
